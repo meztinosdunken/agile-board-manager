41ab5bac8a81bf8ba8420d909551963be7b55a9f
meztinosdunken <orlanmorry5462@hotmail.com>
2025-10-03 11:24:13 -0500

    Added simple middleware to log request details.

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Middleware to log request details
+app.use((req, res, next) => {
+  console.log(`Request Method: ${req.method}, Request URL: ${req.url}`);
+  next();
+});
+
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -16,13 +22,11 @@ mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTop
   .catch(err => console.log(err));
 
 app.get('/', (req, res) => {
-  console.log(`Received request: ${req.method} ${req.url}`);
   res.status(200).send('API is running...');
 });
 
 // Health check endpoint
 app.get('/health', (req, res) => {
-  console.log(`Received request: ${req.method} ${req.url}`);
   res.status(200).json({ status: 'UP' });
 });
 
